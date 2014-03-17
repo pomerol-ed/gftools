8b08a2d1ab7166f42bfc40a734d87eccb706c8a0
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-03-17 01:32:40 -0400

    kmesh : update + test

---
+++ include/kmesh.hpp
@@ -0,0 +1,184 @@
+#ifndef ___GFTOOLS_KMESH_HPP___
+#define ___GFTOOLS_KMESH_HPP___
+
+#include <map>
+#include <numeric>
+
+#include "grid_base.hpp"
+
+namespace gftools { 
+
+class kmesh : public grid_base<real_type, kmesh>
+{
+    mutable real_type domain_len_ = 2.0*PI;
+public:
+    int npoints_;
+    using grid_base<real_type, kmesh>::vals_;
+    kmesh(size_t n_points, real_type len = 2.0*PI);
+    kmesh(const kmesh& rhs):grid_base<real_type, kmesh>(rhs),domain_len_(rhs.domain_len_),npoints_(rhs.npoints_){}
+    kmesh(kmesh &&rhs):grid_base<real_type, kmesh>(rhs),domain_len_(rhs.domain_len_),npoints_(rhs.npoints_){}
+    kmesh():npoints_(0){};
+    kmesh& operator=(kmesh &&rhs) {npoints_ = rhs.npoints_; domain_len_ = rhs.domain_len_; vals_.swap(rhs.vals_); return (*this);};
+    kmesh& operator=(const kmesh &rhs) {npoints_ = rhs.npoints_; domain_len_ = rhs.domain_len_;vals_ = rhs.vals_; return (*this);};
+    std::tuple <bool, size_t, real_type> find (real_type in) const ;
+    template <class Obj> auto integrate(const Obj &in) const ->decltype(in(vals_[0]));
+    //template <class Obj> auto gridIntegrate(std::vector<Obj> &in) const -> Obj;
+    //template <class Obj> auto evaluate(Obj &in, real_type x) const ->decltype(in[0]);
+    //template <class Obj> auto evaluate(Obj &in, point x) const ->decltype(in[0]);
+    point shift(point in, point shift_arg) const;
+    template <class ArgType> point shift(point in, ArgType shift_arg) const;
+    template <class ArgType> real_type shift(real_type in, ArgType shift_arg) const;
+};
+
+struct kmesh_patch : public kmesh 
+{
+    std::map<size_t,size_t> mapvals_;
+public:
+    const kmesh& _parent;
+    size_t _npoints;
+    using kmesh::vals_;
+    kmesh_patch(const kmesh& parent, std::vector<size_t> indices);
+    kmesh_patch(const kmesh& parent);
+    template <class Obj> auto evaluate(Obj &in, real_type x) const ->decltype(in[0]);
+    template <class Obj> auto evaluate(Obj &in, kmesh::point x) const ->decltype(in[0]);
+    size_t get_index(kmesh::point x) const;
+};
+template <>
+inline std::ostream& operator<<(std::ostream& lhs, const num_io< typename kmesh::point> &in){lhs << std::setprecision(in.prec_) << real_type(in.value_.val_); return lhs;};
+template <>
+inline std::istream& operator>>(std::istream& lhs, num_io< typename kmesh::point> &in){real_type v; lhs >> v; in.value_.val_ = v; return lhs;};
+
+//
+// kmesh
+//
+
+inline kmesh::kmesh(size_t n_points, real_type len):
+grid_base<real_type,kmesh>(0,n_points,[n_points,len](size_t in){return len/n_points*in;}),
+domain_len_(len),
+npoints_(n_points)
+{
+}
+/*
+kmesh::kmesh(const kmesh& rhs):grid_base(rhs.vals_),npoints_(rhs._points)
+{
+}
+
+kmesh::kmesh(kmesh &&rhs):grid_base(rhs.vals_),npoints_(rhs.npoints_)
+{
+}
+*/
+inline std::tuple <bool, size_t, real_type> kmesh::find (real_type in) const
+{
+    assert(in>=0 && in < domain_len_);
+    int n = std::lround(in/domain_len_*npoints_);
+    if (n<0) { ERROR("kmesh point is out of bounds, " << in << "<" << 0); return std::make_tuple(0,0,0); };
+    if (n==npoints_) n=0; 
+    if (n>npoints_) { ERROR("kmesh point is out of bounds, " << in << ">" << domain_len_); return std::make_tuple(0,npoints_,0); };
+    real_type weight=in/domain_len_*npoints_-real_type(n);
+    return std::make_tuple (1,n,weight);
+}
+
+/*
+template <class Obj>
+inline auto kmesh::evaluate(Obj &in, real_type x) const ->decltype(in[0]) 
+{
+    const auto find_result=this->find(x);
+    if (!std::get<0>(find_result)) throw (ex_wrong_index()); 
+    return in[std::get<1>(find_result)];
+}
+*/
+
+template <class Obj> 
+auto kmesh::integrate(const Obj &in) const -> decltype(in(vals_[0]))
+{
+    decltype(in(vals_[0])) R = in(real_type(vals_[0]));
+    R=std::accumulate(vals_.begin()+1, vals_.end(), R,[&](decltype(in(vals_[0]))& y,decltype(vals_[0]) & x) {return y+in(x);}); 
+    return R/npoints_;
+}
+
+template <class ArgType>
+inline real_type kmesh::shift(real_type in, ArgType shift_arg) const
+{
+    assert (in>=0 && in < domain_len_);
+    //if (std::abs(real_type(shift_arg))<std::numeric_limits<real_type>::epsilon()) return in;
+    real_type out;
+    out = in + real_type(shift_arg); 
+    out-= std::floor(out/domain_len_)*domain_len_;
+    return out;
+}
+
+
+template <class ArgType>
+inline typename kmesh::point kmesh::shift(point in, ArgType shift_arg) const
+{
+    if (std::abs(real_type(shift_arg))<std::numeric_limits<real_type>::epsilon()) return in;
+    
+    double val = this->shift(in.val_, shift_arg);
+    auto find_result = this->find(val);
+    if (!std::get<0>(find_result)) throw (ex_wrong_index());
+    size_t index = std::get<1>(find_result);
+    return (*this)[index];
+}
+
+inline typename kmesh::point kmesh::shift(point in, point shift_arg) const
+{
+    size_t index = (in.index_ + shift_arg.index_)%npoints_;
+    #ifndef NDEBUG
+    real_type val = this->shift(in.val_, shift_arg.val_);
+    if (std::abs(val - vals_[index])>1e-3) throw (ex_wrong_index()); 
+    #endif
+    //out.val_ = vals_[out.index_];
+    return vals_[index];
+}
+
+
+
+//
+// kmesh_patch
+//
+
+
+inline kmesh_patch::kmesh_patch(const kmesh& parent, std::vector<size_t> indices):
+    kmesh(indices.size()),
+    _parent(parent),
+    _npoints(indices.size())
+{
+    for (size_t i=0; i<_npoints; ++i) {
+        vals_[i]=_parent[indices[i]]; 
+        mapvals_[size_t(vals_[i])] = i;
+        }
+}
+
+inline kmesh_patch::kmesh_patch(const kmesh& parent):
+    _parent(parent),
+    _npoints(parent.size())
+{
+    vals_ = parent.get_points();
+     for (size_t i=0; i<_npoints; ++i) {
+        mapvals_[size_t(vals_[i])] = i;
+        }
+}
+
+template <class Obj> 
+inline auto kmesh_patch::evaluate(Obj &in, real_type x) const ->decltype(in[0])
+{
+    const auto find_result=_parent.find(x);
+    if (!std::get<0>(find_result)) throw (ex_wrong_index()); 
+    return evaluate(in, kmesh::point(std::get<1>(find_result), x));
+}
+
+template <class Obj> 
+inline auto kmesh_patch::evaluate(Obj &in, kmesh::point x) const ->decltype(in[0])
+{
+    return in[this->get_index(x)];
+}
+
+inline size_t kmesh_patch::get_index(kmesh::point x) const
+{
+    auto f1 = mapvals_.find(size_t(x));
+    if (f1!=mapvals_.end()) { return f1->second; }
+    else throw (ex_wrong_index());
+}
+
+} // end of namespace gftools
+#endif // endif :: ifndef ___GFTOOLS_KMESH_HPP___
---
+++ test/kmesh_test.cpp
@@ -0,0 +1,27 @@
+
+#include "kmesh.hpp"
+
+using namespace gftools;
+int main()
+{
+    kmesh k1(32);
+    std::cout << k1 << std::endl;
+
+    std::function<real_type(real_type)> F1=[](real_type x) {return cos(x);};
+    real_type outF1 = k1.integrate(F1);
+    std::cout << "sum_k cos(k) = " << outF1 << std::endl;
+    if (!tools::is_float_equal(outF1,0.,1e-8)) return EXIT_FAILURE;
+
+    typedef typename kmesh::point point;
+    point p1 = k1.find_closest(PI);
+    std::cout << "PI == " << p1 << std::endl;
+    if (!tools::is_float_equal(PI,double(p1),1e-8)) return EXIT_FAILURE;
+
+    kmesh_patch patch1(k1, {0, 2, 4, 8, 14, 16, 30});
+    std::cout << "kmesh patch : " << patch1 << std::endl;
+    std::vector<int> data_y = {0,1,2};
+    std::cout << "y[" << k1[2] << "]=" << patch1.evaluate(data_y,k1[2]) << std::endl;
+    if (patch1.evaluate(data_y,k1[2]) != 1) return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
