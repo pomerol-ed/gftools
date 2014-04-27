8be4eea5b21f43ac624b08b5dbdd477e094097fa
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-04-27 02:13:52 -0400

    added gftools.hpp

---
+++ gftools.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "gftools/definitions.hpp" 
+#include "gftools/num_io.hpp"
+
+#include "gftools/grid_base.hpp"
+#include "gftools/matsubara_grid.hpp"
+#include "gftools/kmesh.hpp"
+#include "gftools/real_grid.hpp"
+#include "gftools/enumerate_grid.hpp"
+
+#include "gftools/container.hpp"
+
+#include "gftools/grid_object.hpp"
