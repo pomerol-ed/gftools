23fccc556ef0ad330e8d9dc835fb88e1c20f4221
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-04-29 22:37:36 -0400

    gftools.hpp update

--- gftools.hpp
+++ gftools.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "gftools/definitions.hpp" 
+#include "gftools/defaults.hpp" 
 #include "gftools/num_io.hpp"
 
 #include "gftools/grid_base.hpp"
 #include "gftools/matsubara_grid.hpp"
 #include "gftools/kmesh.hpp"
 #include "gftools/real_grid.hpp"
-#include "gftools/enumerate_grid.hpp"
+#include "gftools/enum_grid.hpp"
 
 #include "gftools/container.hpp"
 
