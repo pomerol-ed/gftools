e0a4de1a46e08c9822488c0d83adc12b9bb7bae2
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-05-18 02:27:43 -0400

    implemented evaluate expression

--- gftools/eval_expression.hpp
+++ gftools/eval_expression.hpp
@@ -1,68 +1,68 @@
 #pragma once
 
-#include "grid_tools.hpp"
+#include <utility>
+#include <array>
+
+//#include "tuple_tools.hpp" // only for debug
 
 namespace gftools { 
 
-// eval some object on a grid
+/** eval_expression is an unary tree, representing operator[] of a multidimensional object V, 
+  * which has a []...[] method of depth N. The evaluation takes place in 3 walks (forward-back-forward) in the tree,
+  * using operator[], apply and get methods. operator[] returns a child eval_expression of depth D=N-1. 
+  * When the depth D reaches 1, child calls apply(indices) method of a parent, passing the index of [] operator to it. 
+  * Going back to the the root of the tree (N=D) apply method collects an array of indices. 
+  * After that this array is passed to the static method get(data,indices) which calls operator [] of 
+  * V recursively until D=1, gaining in the end the value of V at requested indices.
+*/
+template <typename V, typename VType, size_t D, size_t N = D> struct eval_expression 
+{
+    /// Typedef for underlying expression.
+    typedef eval_expression<eval_expression<V,VType,D,N>, VType, D-1,N> under_type;
+    /// Typedef of underylung value type of V
+    typedef VType value_type;
 
-template <typename Node, typename, typename Evaluable = Node> struct grid_eval_expr; 
+    /// Construct from data object or eval_expression parent
+    eval_expression(V v):v_(std::forward<V>(v)){};
+    /// Returns a child - underlying expression of the order of D-1
+    under_type operator[](int x) { i_ = x; return under_type(*this); }
+    /// Combine indices from [] operator using apply method
+    template <size_t M = N>
+        typename std::enable_if<(M!=D), VType&>::type apply(std::array<int, N>&& indices){
+            indices[N-D] = i_; return v_.apply(std::move(indices)); }
+    /// Call [] operator of V using combined indices from eval_expression::apply method
+    template <size_t M = N>
+        typename std::enable_if<(M==D), VType&>::type apply(std::array<int, N>&& indices){
+            indices[0] = i_; return this->get(v_, indices);
+        };
 
-template <typename Node, typename ...GridTypes, typename Evaluable> 
-struct grid_eval_expr<Node, std::tuple<GridTypes...>, Evaluable>
-{ 
-    constexpr static size_t D = sizeof...(GridTypes);
-    typedef tools::grid_tuple_traits<std::tuple<typename std::remove_reference<GridTypes>::type...>> trs;
-    typedef typename std::tuple<typename std::add_const<typename std::add_lvalue_reference<GridTypes>::type>::type...> grid_constref_tuple; 
-    typedef typename trs::grid_tuple_type grid_tuple_type;
-    typedef typename trs::arg_tuple arg_tuple;
-    typedef typename std::remove_reference<decltype(std::declval<Evaluable>()[0])>::type data_type;
-    typedef grid_eval_expr< 
-        grid_eval_expr<Node, std::tuple<GridTypes...>> const&, 
-        decltype(tuple_tools::tuple_tail(std::declval<grid_constref_tuple>())),
-        data_type
-    > child_t;
+    /// Pass indices, collected during first walk through the tree with [] and apply methods to V and use 
+    template <typename V2>
+    static VType& get(V2&& d, std::array<int, N> indices) { return under_type::get(d[indices[N-D]], indices); }
 
-    Node f_;
-    grid_constref_tuple grids_;
+    /// Data or parent expression
+    V v_; 
+    /// Index of operator[]
+    int i_;
+};
 
-    grid_eval_expr() = delete;
-    grid_eval_expr(Node F, grid_constref_tuple grids):
-        f_(std::forward<Node>(F)),
-        grids_(grids)
-    {
-    DEBUG(D);
-    }
+template <typename V, typename VType, size_t N> struct eval_expression<V,VType,1,N>
+{
+    /// Construct from data object or eval_expression parent
+    eval_expression(V v):v_(std::forward<V>(v)){};
+    /// return value of v[x] for 1d objects.
+    template <size_t M = N>
+        typename std::enable_if<(M==1), VType&>::type operator[] (int x){return v_[x];}
+    /// pass the index of operator[] to parent apply method if depth != 1
+    template <size_t M = N>
+        typename std::enable_if<(M!=1), VType&>::type operator[] (int x){std::array<int, N> ind; ind[N-1] = x; return v_.apply(std::move(ind));}
 
-    grid_eval_expr(grid_eval_expr const&) = delete;
-    grid_eval_expr& operator= (grid_eval_expr const&) = delete;
+    /// Use collected indices to extract value from V
+    template <typename V2>
+        static VType& get(V2&& d, std::array<int, N> indices) { return d[indices[N-1]]; }
 
-    grid_eval_expr(grid_eval_expr&&) = default;
-    grid_eval_expr& operator= (grid_eval_expr&&) = default;
-
-    //template <bool Root = (std::is_same<Node,Evaluable>::value), typename std::enable_if<!Root>::type>
-
-    template <typename T = Node, typename std::enable_if<!std::is_same<T,Evaluable>::value>::type>
-    auto apply (std::array<size_t, D-1> p, size_t i) -> decltype(f_.apply({{i}}, 0)) {}
-
-    template <typename T = Node, typename std::enable_if<std::is_same<Node,Evaluable>::value>::type>
-    auto apply (std::array<size_t, D-1> p, size_t i) const -> int {} ;//decltype(f_.apply({{i}}, 0)) {}
-
-    template <int D1 = D>
-    typename std::enable_if<(D1>1),child_t>::type
-    operator[] (size_t index) const { 
-        return child_t(*this,tuple_tools::tuple_tail(grids_)); 
-    }
-
-    template <int D1 = D>
-    typename std::enable_if<(D1==1), data_type>::type
-    operator[] (size_t index) const { 
-        return apply({{}}, index);
-    }
-    
-
-    //auto eval(arg_tuple_type in) -> child_t const;
-
+    /// Data or parent expression
+    V v_;
 };
 
 };
---
+++ test/eval_expression_test.cpp
@@ -0,0 +1,106 @@
+#include <numeric>
+
+#include "eval_expression.hpp"
+#include "container.hpp"
+#include "real_grid.hpp"
+#include "grid_object.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace gftools;
+
+TEST(expression1d, vector) {
+    std::vector<double> y(10);
+    std::iota(y.begin(), y.end(), 0.3);
+    eval_expression<std::vector<double>, double, 1> e(y);
+    std::cout <<  y[2] << "==" << e[2] << std::endl;
+    for (int i=0; i<y.size(); ++i) { 
+        ASSERT_EQ(y[i], e[i]);
+        };
+}
+
+TEST(expression1d, container) {
+    container<double, 1> y(10);
+    std::iota(&y[0], &y[9], 0.3);
+    eval_expression<container<double, 1>, double, 1> e(y);
+    std::cout <<  y[2] << "==" << e[2] << std::endl;
+    for (int i=0; i<y.size(); ++i) { 
+        ASSERT_EQ(y[i], e[i]);
+        };
+}
+
+TEST(expression2d, vector) {
+    std::array<size_t, 2> dims = {{10, 4}};
+    typedef std::vector<std::vector<double>> data2d;
+    data2d y(dims[0]);
+    for (auto& x : y) { x.resize(dims[1]); std::iota(x.begin(), x.end(), 1.2); };
+    eval_expression<data2d, double, 2> e(y);
+    std::cout << "original   : " << y[2][3] << std::endl;
+    std::cout << "expression : " << e[2][3] << std::endl;
+    for (int i1 = 0; i1 < dims[0]; i1++) 
+        for (int i2 = 0; i2 < dims[1]; i2++) 
+            ASSERT_EQ(y[i1][i2], e[i1][i2]);
+}
+
+TEST(expression2d, container) {
+    typedef container<double, 2> data2d;
+    std::array<size_t, 2> dims = {{10, 4}};
+    data2d y(dims);
+    std::iota(&y[0][0], &y[dims[0]-1][dims[1]-1], 0.2);
+    eval_expression<data2d, double, 2> e(y);
+    std::cout << "original   : " << y[2][3] << std::endl;
+    std::cout << "expression : " << e[2][3] << std::endl;
+    for (int i1 = 0; i1 < dims[0]; i1++) 
+        for (int i2 = 0; i2 < dims[1]; i2++) 
+            ASSERT_EQ(y[i1][i2], e[i1][i2]);
+    };
+
+TEST(expression2d, grid_object) {
+    typedef grid_object<double, real_grid, real_grid> data2d;
+    std::array<size_t, 2> dims = {{10, 4}};
+    data2d y(real_grid(0,1.,dims[0]), real_grid(0,1.,dims[1]));
+    y.fill(typename data2d::function_type([](double x, double y){return sin(x+y);}));
+    eval_expression<data2d, double, 2> e(y);
+    std::cout << "original   : " << y[2][3] << std::endl;
+    std::cout << "expression : " << e[2][3] << std::endl;
+    for (int i1 = 0; i1 < dims[0]; i1++) 
+        for (int i2 = 0; i2 < dims[1]; i2++) 
+            ASSERT_EQ(y[i1][i2], e[i1][i2]);
+    };
+
+
+TEST(expression3d, vector) {
+    typedef std::vector<std::vector<std::vector<double>>> data3d;
+    std::array<size_t, 3> dims = {{10, 4, 6}};
+    data3d y(dims[0]);
+    for (auto& x : y) { x.resize(dims[1]); for (auto& z: x) { z.resize(dims[2]); std::iota(z.begin(), z.end(), 1.2); };};
+    eval_expression<data3d, double, 3> e(y);
+    std::cout << "original   : " <<  y[2][3][5] << std::endl;
+    std::cout << "expression : " <<  e[2][3][5] << std::endl;
+    for (int i1 = 0; i1 < dims[0]; i1++) 
+        for (int i2 = 0; i2 < dims[1]; i2++) 
+            for (int i3 = 0; i3 < dims[2]; i3++) 
+                ASSERT_EQ(y[i1][i2][i3], e[i1][i2][i3]);
+}
+
+TEST(expression3d, container) {
+    typedef container<double, 3> data3d;
+    std::array<size_t, 3> dims = {{10, 4, 6}};
+    data3d y(dims);
+    std::iota(&y[0][0][0], &y[dims[0]-1][dims[1]-1][dims[2]-1], 0.1);
+    eval_expression<data3d, double, 3> e(y);
+    std::cout << "original   : " <<  y[2][3][5] << std::endl;
+    std::cout << "expression : " <<  e[2][3][5] << std::endl;
+    for (int i1 = 0; i1 < dims[0]; i1++) 
+        for (int i2 = 0; i2 < dims[1]; i2++) 
+            for (int i3 = 0; i3 < dims[2]; i3++) 
+                ASSERT_EQ(y[i1][i2][i3], e[i1][i2][i3]);
+}
+
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hi!" << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
