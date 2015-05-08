3e5a6967973a4f9faa4b72ae080f783ff3926da1
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2015-05-08 15:43:35 -0400

    verbose description of almost_equal

--- gftools/almost_equal.hpp
+++ gftools/almost_equal.hpp
@@ -8,23 +8,29 @@
 
 namespace gftools {
 
+/// "almost equal" comparison - performs tolerance-based comparison for provided objects
+///TODO: note duplication of functionality with the more fancy is_float_equal function in tools.
+template <typename T1,typename T2> 
+bool almost_equal(T1 t1, T2 t2, real_type tol = 10.*std::numeric_limits<real_type>::epsilon());
+
 /// A helper structure to perform type-dependent "almost equal" comparison
 /// The usage of this structure allows to make comparison for user-defined types, in which
 /// case the template specialization of almost_equal_checker should be provided
 template <typename, typename > struct almost_equal_checker;
 
-/// "almost equal" comparison - performs tolerance-based comparison for provided objects
-///TODO: note duplication of functionality with the more fancy is_float_equal function in tools.
-template <typename T1,typename T2>
-    bool almost_equal(T1 t1, T2 t2, real_type tol = 10.*std::numeric_limits<real_type>::epsilon()){ 
-    return almost_equal_checker<T1,T2>::is_equal(t1,t2,tol); 
-}
-
 /// A helper structure that defines a "true_type" compile-time constant if a given type can be used as a number
 /// In this realization everything that can be casted to a complex_number is a number.
+/// TODO : replace complex conversion comparison
 template <typename T> struct is_number;
 
 // implementations
+template <typename T1,typename T2>
+    bool almost_equal(T1 t1, T2 t2, real_type tol) {
+    return almost_equal_checker<T1,T2>::is_equal(t1,t2,tol); 
+}
+
+
+// TODO : remove complex_type conversion, use a compile time check only
 template <typename T1, typename T2 = T1>
 struct almost_equal_checker {
     static typename std::enable_if<is_number<T1>::value && is_number<T2>::value, bool>::type is_equal(T1 t1, T2 t2, real_type tol) { 
