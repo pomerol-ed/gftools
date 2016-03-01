42d5e75132ba708ca9e51198b993d87938cc4557
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2016-03-01 12:47:42 -0500

    df fk 2d example

--- example/df_fk_2d.cpp
+++ example/df_fk_2d.cpp
@@ -35,25 +35,56 @@ int main(int argc, char *argv[])
     double beta = vm["beta"].as<double>(); 
     double U = vm["U"].as<double>(); 
 
+    std::cout << "!" << std::endl;
+
     static constexpr int NDim = 2; // work in 2 dimensions
+    int kpts = 16;
 
     typedef grid_object<std::complex<double>, fmatsubara_grid, fmatsubara_grid> vertex_type;
     typedef grid_object<std::complex<double>, fmatsubara_grid> gw_type;
     typedef grid_object<std::complex<double>, fmatsubara_grid, kmesh, kmesh> gk_type;
-    typedef grid_object<std::complex<double>, kmesh, kmesh> ek_type;
+    typedef grid_object<std::complex<double>, kmesh, kmesh> disp_type;
     typedef Eigen::MatrixXcd matrix_type; 
 
+    double T = 1.0/beta;
+    std::cout << "beta = " << beta << std::endl;
+    std::cout << "T = " << T << std::endl;
+
+    // Prepare evaluation grids
     fmatsubara_grid fgrid(-20,20,beta);
 
+    // Hybridization function (atomic limit)
     gw_type delta(fgrid);
     delta.fill([&](std::complex<double> w){return double(2*NDim) / w; });
+    // Local gf (atomic limit)
+    gw_type gw(fgrid);
+    gw.fill([&](std::complex<double> w){return 0.5/(w - U/2.0) + 0.5/(w + U/2.0);});
 
+    // Local vertex (atomic limit)
     vertex_type gamma4(std::make_tuple(fgrid, fgrid));
-
     typename gw_type::function_type Lambda = [U](std::complex<double> w){return 1. - U*U/4./w/w;};
     gamma4.fill([&](std::complex<double> w1, std::complex<double> w2){ 
-            return beta * U * U / 4.0 * double(1 - (w1.index() == w2.index()) ) * (1. - U*U/4./w1/w1) * (1. - U*U/4./w2/w2) 
+            return beta * U * U / 4.0 * double(1 - tools::is_float_equal(w1, w2) ) * (1. - U*U/4./w1/w1) * (1. - U*U/4./w2/w2); 
         });
+
+    // k-dependent input
+    // define a mesh in Brilloin zone (BZ)
+    kmesh kgrid(kpts);
+    int totalkpts = std::pow(kpts, NDim);
+    std::cout << "kmesh : " << kpts << " points " << std::endl; 
+    std::cout << "total pts in BZ = " << totalkpts << std::endl; 
+
+    // lattice dispersion
+    disp_type eps(std::forward_as_tuple(kgrid, kgrid));
+    eps.fill([&](double kx, double ky){return -2*(cos(kx) + cos(ky));});
+
+    // lattice gf in dmft
+    gk_type glat_dmft(std::forward_as_tuple(fgrid, kgrid, kgrid));
+    glat_dmft.fill([&](fmatsubara_grid::point w, kmesh::point kx, kmesh::point ky){return 1.0 / (1.0 / gw(w) + delta(w) - eps(kx, ky));});
+
+    // bare dual df
+    gk_type gd0(glat_dmft.grids());
+    gd0.fill([&](fmatsubara_grid::point w, kmesh::point kx, kmesh::point ky){return glat_dmft(w, kx, ky) - gw(w);});
     
     // define full vertex (will be evaluated below)
     vertex_type full_vertex(gamma4);
@@ -61,28 +92,14 @@ int main(int argc, char *argv[])
     matrix_type gamma4_matrix = gamma4.data().as_matrix();
     matrix_type full_vertex_matrix(gamma4_matrix);
 
-
-    double beta = fgrid.beta(); 
-    double T = 1.0/beta;
-    std::cout << "beta = " << beta << std::endl;
-    std::cout << "T = " << T << std::endl;
-    // load bare dual gf
-    std::cout << "Loading gd0 from " << gd0_file << std::endl;
-    gk_type gd0 = loadtxt<gk_type>(gd0_file);
     // check that we are consistent - 2 frequency grids are the same
     if (gd0.template grid<0>() != fgrid) throw std::logic_error("matsubara grid mismatch");
     
-    // define mesh in Brilloin zone (BZ)
-    const kmesh& kgrid = gd0.template grid<1>();
-    int kpts = kgrid.size();
-    int totalkpts = std::pow(kpts, NDim);
-    std::cout << "kmesh : " << kpts << " points " << std::endl; 
-    std::cout << "total pts in BZ = " << totalkpts << std::endl; 
     std::cout << "kmesh : " << kgrid << std::endl;
     // Initalize dual self-energy on the same grids as gd0
     gk_type sigma_dual(gd0.grids());
     sigma_dual = 0.0;
-    ek_type bare_bubbles(kgrid,kgrid);
+    disp_type bare_bubbles(kgrid,kgrid);
 
     // now loop through the BZ (no irreducible part optimization)
     for (kmesh::point q1 : kgrid.points()) { 
@@ -116,7 +133,7 @@ int main(int argc, char *argv[])
     sigma_dual.savetxt("sigma_wk.dat");
     // save sigma at first matsubara
     auto w0 = fgrid.find_nearest(I*PI/beta);
-    ek_type sigma_w0(std::forward_as_tuple(kgrid,kgrid),sigma_dual[w0]);
+    disp_type sigma_w0(std::forward_as_tuple(kgrid,kgrid),sigma_dual[w0]);
     sigma_w0.savetxt("sigma_w0.dat");
     // save bare bubbles
     bare_bubbles.savetxt("db0.dat");
